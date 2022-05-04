25d873308c9de703109cbec0b8ef20c0f6628ac9
Mazen-Aboulkhair <mazentarek1412@gmail.com>
2022-05-04 22:58:29 +0200

    Mazen-login-style

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import './Login.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTree } from '@fortawesome/free-solid-svg-icons'
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
@@ -12,49 +12,40 @@ class Login extends Component {
           <div class="shape"></div>
       </div>
         <form className="form">
-          <div className="text-login">
-            <h3>Login</h3>
-          </div>
-          <div className="input">
-            <label htmlFor="user">Username :</label>
-            <input
-              id="user"
-              type="text"
-              placeholder="write your username"
-              name="username"
-            />
-          </div>
-          <div className="input">
-            <label htmlFor="pass">Password :</label>
-            <input
-              id="pass"
-              type="password"
-              placeholder="write your password"
-              name="password"
-            />
-          </div>
-          <Link to="/forgetpass">
-            <button className="btn-forget">Forget Password?</button>
-          </Link>
-          <div className="btns-login">
-            <Link className="save" to="/submit">
-              <button className="btn-login">Login</button>
-            </Link>
-            <div className="icon-google">
-            <FontAwesomeIcon className='icon' icon={faTree}/>
+              <h3>Login</h3>
+        <div class="input-field">
+               <input type="text" required></input>
+               <label>Username</label>
             </div>
-            <Link className="SignUp" to="/SignUp">
-              <button className="btn-signup">Creat an account</button>
-            </Link>
-          </div>
-          <div className='or-block'> 
-        <hr className='lines' size="1"></hr>
-        <div className='or'>Or</div>
-        <hr className='lines' size="1"></hr>
-          </div>
+            <div class="input-field">
+               <input class="pswrd" type="password" required></input>
+               <span class="show">SHOW</span>
+               <label>Password</label>
+            </div>
+           
+      <Link to="/forgetpass">
+        <button className="btn-forget">Forget Password?</button>
+      </Link>
+      <div className="btns-login">
+        <Link className="save" to="/submit">
+          <button className="btn-login">Login</button>
+        </Link>
+        <div className="icon-google">
+        {/* <FontAwesomeIcon className='icon' icon={faTree}/> */}
+        </div>
+        <Link className="SignUp" to="/SignUp">
+          <button className="btn-signup">Creat an account</button>
+        </Link>
+      </div>
+      <div className='or-block'> 
+    <hr className='lines' size="1"></hr>
+    <div className='or'>Or</div>
+    <hr className='lines' size="1"></hr>
+      </div>
           </form>
         </div>
          );
+         
   }
 }
 
