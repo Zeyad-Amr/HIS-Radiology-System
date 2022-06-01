82bdeea6e84e99b5aebd24939e759165a0e8a56e
omaranwar21 <omarahmed11185@gmail.com>
2022-06-02 01:15:11 +0200

    Login Pre-Design finishing

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,11 +1,11 @@
 import "./Login.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
-import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import {useState} from "react";
 import { Link } from "react-router-dom";
 import axios from "../API/axios";
+import {ImgLeft} from "../Home/images/img.svg";
 
 function Login() {
 
@@ -22,61 +22,43 @@ function Login() {
   
 
   return (
-    <div
-      className="container"
-      style={{ margin: "0", width: "100%", maxWidth: "100%" }}
-    >
-      <div className="background">
-        <div class="shape"></div>
-        <div class="shape"></div>
-      </div>
-      <form className="form" onSubmit={handleSubmit}>
-        <h3>Login</h3>
-        <FontAwesomeIcon
-          className="icon-login"
-          icon={faArrowCircleRight}
-          size="2x"
-        />
-        <div class="input-field-L">
-          <input
-            type="text"
-            id="username"
-            required
-            onChange={(e) => setUser(e.target.value)}
-            value={user}
-          ></input>
-          <label>Username</label>
-        </div>
-        <div class="input-field-L">
-          <input
-            class="pswrd"
-            type="password"
-            required
-            id="password"
-            onChange={(e) => setPwd(e.target.value)}
-            value={pwd}
-          ></input>
-          <span class="show">SHOW</span>
-          <label>Password</label>
-        </div>
-        <Link to="/forgetpass">
-          <button className="btn-forget">Forget Password?</button>
-        </Link>
-        <div className="btns-login">
-            <button className="btn-login">Login</button>
-          <div className="icon-google">
-            <FontAwesomeIcon className="icon" icon={faGoogle} size="2x"/>
-          </div>
-          <Link className="SignUp" to="/SignUp">
-            <button className="btn-signup">Creat an account</button>
-          </Link>
-        </div>
-        <div className="or-block">
-          <hr className="lines" size="1"></hr>
-          <div className="or">Or</div>
-          <hr className="lines" size="1"></hr>
-        </div>
-      </form>
+    <div className="login">
+
+          <section class="side">
+              <img src= {ImgLeft} alt=""/>
+          </section>
+
+          <section class="main">
+              <div class="login-container">
+                  <p class="title">Welcome back</p>
+                  <div class="separator"></div>
+                  <p class="welcome-message">Please, provide login credential to proceed and have access to all our services</p>
+
+                  <form class="login-form">
+                      <div class="form-control">
+                          <input type="text" placeholder="Username"/>
+                          <i class="fas fa-user"></i>
+                      </div>
+                      <div class="form-control">
+                          <input type="password" placeholder="Password"/>
+                          <i class="fas fa-lock"></i>
+                      </div>
+                      <button class="submit">Login</button>
+                      <div className="other">
+                        <a href="">Forget Password?</a>
+                        <a href="">Creat an account</a>
+                      </div>
+                      <div id="google">
+                        <h5>or</h5>
+                        <h5>Login with</h5>
+                        <a href="#" class="facebook">
+                          <FontAwesomeIcon icon={faGoogle} color="white" size="2x"/>
+                        </a>
+                      </div>
+                  </form>
+              </div>
+          </section>
+
     </div>
   );
 }
