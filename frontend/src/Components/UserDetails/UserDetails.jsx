f72f8277e3c8e17b52d47207cae6d72ed097db37
omaranwar21 <omarahmed11185@gmail.com>
2022-06-05 22:16:25 +0200

    data display style edits

--- frontend/src/Components/UserDetails/UserDetails.jsx
+++ frontend/src/Components/UserDetails/UserDetails.jsx
@@ -23,78 +23,80 @@ function UserDetails() {
 
   return (
     <div className="log">
-      <section>
-        <form className="signup-form">
-          <div className="cont-img">
-            <div className="input-field-cont">
-              <div class="input-field">
-                <input type="text" required value={data.fname}></input>
-                <label>First Name</label>
-              </div>
-              <div class="input-field">
-                <input type="text" required value={data.lname}></input>
-                <label>Last Name</label>
-              </div>
-              <div class="input-field">
-                <input type="text" required value={data.username}></input>
-                <label>Username</label>
-              </div>
-              <div class="input-field">
-                <input type="text" required value={data.email}></input>
-                <label>Email</label>
-              </div>
-              <div class="input-field">
-                <input type="text" required value={data.SSN}></input>
-                <label>National ID</label>
-              </div>
-              <div class="input-field">
-                <input type="text" required value={data.country}></input>
-                <label>Country</label>
-              </div>
-              <div class="input-field">
-                <input type="text" required value={data.address}></input>
-                <label>Address</label>
-              </div>
-              <div class="input-field">
-                <input type="text" required value={data.phone}></input>
-                <label>Phone number</label>
-              </div>
-              <div className="input-field">
-                <input
-                  id="birthdate"
-                  type="text"
-                  name="birthdate"
-                  className="birthdate"
-                  placeholder=""
-                  value={data.bdate}
-                ></input>
-                <label htmlFor="birthdate">Birthdate</label>
-              </div>
-              <div class="input-field">
-          <label className="input-field-cont-1">Gender</label>
-          <select class="gender-selection" >
-            <option value="Choose Gender" disabled selected hidden>{data.gender}</option>
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-          </select>
-        </div>
-        <div class="input-field">
-        <label className="input-field-cont-1">Role</label>
-        <select class="gender-selection" >
-          <option value="Choose Role" disabled selected hidden>{data.role}</option>
-          <option value="patient">patient</option>
-          <option value="Doctor">Doctor</option>
-        </select>
-      </div>
-            </div>
+      <form className="signup-form" id='dataDisplay'>
+
+        <div id='dataDisplay3'>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.fname}></input>
+            <label>First Name</label>
           </div>
-        </form>
-      </section>
-      <section>
-        <form className="signup-form">
-          <div className="cont-img">
-            <div className="input-field-cont">
-            <div class="input-field">
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.lname}></input>
+            <label>Last Name</label>
+          </div>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.username}></input>
+            <label>Username</label>
+          </div>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.email}></input>
+            <label>Email</label>
+          </div>
+        </div>
+
+        <div id='dataDisplay3'>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.SSN}></input>
+            <label>National ID</label>
+          </div>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.phone}></input>
+            <label>Phone number</label>
+          </div>
+          <div className="input-field" id="inputField">
+            <input
+              id="birthdate"
+              type="text"
+              name="birthdate"
+              className="birthdate"
+              placeholder=""
+              value={data.bdate}
+            ></input>
+            <label htmlFor="birthdate">Birthdate</label>
+          </div>
+          <div class="input-field" id="inputField">
+            <label className="input-field-cont-1" id="inputField">Gender</label>
+            <select class="gender-selection" >
+              <option value="Choose Gender" disabled selected hidden>{data.gender}</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+          </div>
+        </div>
+
+        <div id='dataDisplay3'>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.country}></input>
+            <label>Country</label>
+          </div>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.address}></input>
+            <label>Address</label>
+          </div>
+          <div class="input-field" id="inputField">
+            <label className="input-field-cont-1">Role</label>
+            <select class="gender-selection" >
+              <option value="Choose Role" disabled selected hidden>{data.role}</option>
+              <option value="patient">patient</option>
+              <option value="Doctor">Doctor</option>
+            </select>
+          </div>
+        </div>
+      </form>
+
+      <form className="signup-form" id='dataDisplay'>
+        <div id='dataDisplay3'>
+          <div class="input-field" id="inputField">
             <label className="input-field-cont-1">Department</label>
             <select class="gender-selection">
               <option value="Choose Department" disabled selected hidden>{data.dep}</option>
@@ -102,22 +104,22 @@ function UserDetails() {
               <option value="Lab">Lab</option>
             </select>
           </div>
-              <div class="input-field">
-                <input type="text" required value={data.salary}></input>
-                <label>Salsry</label>
-              </div>
-              <div class="input-field">
-          <label className="input-field-cont-1">Shift</label>
-          <select class="gender-selection">
-            <option value="Choose Shift" disabled selected hidden>{data.shift}</option>
-            <option value="Day">Day</option>
-            <option value="Night">Night</option>
-          </select>
-        </div>
-            </div>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.salary}></input>
+            <label>Salary</label>
           </div>
-        </form>
-      </section>
+          <div class="input-field" id="inputField">
+            <label className="input-field-cont-1">Shift</label>
+            <select class="gender-selection">
+              <option value="Choose Shift" disabled selected hidden>{data.shift}</option>
+              <option value="Day">Day</option>
+              <option value="Night">Night</option>
+            </select>
+          </div>
+        </div>
+      </form>
+
+
     </div>
   );
 }
