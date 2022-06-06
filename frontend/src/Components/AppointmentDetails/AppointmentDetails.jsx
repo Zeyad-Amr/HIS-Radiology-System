5a136476c3198b8c2f4cb82bc8a84580122cd706
momen882001 <momen2001.mm@gmail.com>
2022-06-06 17:32:52 +0200

    AppointmentDetails Structure & css

---
+++ frontend/src/Components/AppointmentDetails/AppointmentDetails.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import Button from '../Button/Button';
+
+function AppointmentDetails() {
+
+    const data = 
+        {
+            start_time: "2022-06-04",
+            end_time: "2022-06-04",
+            services_name: "null",
+            device_name: "null",
+        };
+
+        function handleClick(event) {
+            event.preventDefault();
+            alert('Button Clicked');
+        };
+
+  return (
+    <form className="signup-form">
+    <div className="serv-cont">
+      <div className="text" style={{    textAlign: "center",
+        fontSize: "xx-large"}}>Appointment Details</div>
+      <div class="input-field">
+          <input type="text" required value={data.start_time}></input>
+          <label>StartTime</label>
+        </div>
+        <div class="input-field">
+        <input type="text" required value={data.end_time}></input>
+        <label >EndTime</label>
+      </div>
+        <div class="input-field">
+          <label>Devices</label>
+          <select class="gender-selection">
+            <option value="Choose Device" disabled selected hidden>
+              {data.device_name}
+            </option>
+            <option value="CT">CT</option>
+            <option value="MRI">MRI</option>
+          </select>
+        </div>
+        <div class="input-field">
+          <label>Services</label>
+          <select class="gender-selection">
+            <option value="Choose Service" disabled selected hidden>
+              {data.services_name}
+            </option>
+            <option value="CT">Akwa</option>
+            <option value="MRI">Mix</option>
+          </select>
+        </div>
+        <Button
+        onClick={handleClick}
+          value='Click me!'
+        />
+        </div>
+    </form>
+  )
+}
+
+export default AppointmentDetails
