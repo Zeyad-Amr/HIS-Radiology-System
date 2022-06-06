048ad43f13c49d60d17c2b081cd031cc7d6ecfbb
momen882001 <momen2001.mm@gmail.com>
2022-06-06 07:19:16 +0200

    OrderDetails structure & css

--- frontend/src/Components/OrderDetails/OrderDetails.jsx
+++ frontend/src/Components/OrderDetails/OrderDetails.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './OrderDetails.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHourglassStart, faUser } from "@fortawesome/free-solid-svg-icons";
@@ -6,17 +6,32 @@ import * as GiIcons from "react-icons/gi";
 import * as HiIcons from "react-icons/hi";
 import * as AiIcons from "react-icons/ai";
 import * as TbIcons from "react-icons/tb";
+import Button from '../Button/Button';
 
 
 function OrderDetails() {
-  // const [step , setStep] = useState(1);
-  var [step, setSidebar] = useState(1);
+
+    const data = {
+    receptionist_id: "66",
+    patient_id: "30",
+    services_id: "2",
+    result_id: "null",
+    create_time: "2022-06-02",
+    status: "null"
+    }
+
+    function handleClick(event) {
+        event.preventDefault();
+        alert('Button Clicked');
+    };
+
   return (
     <div>
 
       <section className="vh-100" >
-        <div className="container py-5 h-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="container py-1 h-80">
+        <div className='text' id='order'>Order Details</div>
+          <div className="row d-flex justify-content-center align-items-flex-start h-50">
             <div className="col-12">
               <div className="card card-stepper text-black">
 
@@ -33,11 +48,11 @@ function OrderDetails() {
                   </div>
 
                   <ul id="progressbar-2" className="d-flex justify-content-between mx-0 mt-0 mb-5 px-0 pt-0 pb-2">
-                    <li className= { step >= 1 ? "holding1" : ""} id="step1"></li>
-                    <li className= { step >= 2 ? "holding2" : ""} id="step2"></li>
-                    <li className= { step >= 3 ? "holding3" : ""} id="step3"></li>
-                    <li className= { step >= 4 ? "holding4" : ""} id="step4"></li>
-                    <li className= { step >= 5 ? "holding5" : ""} id="step5"></li>
+                    <li className="step0 active text-center" id="step1"></li>
+                    <li className="step0 active text-center" id="step2"></li>
+                    <li className="step0 active text-center" id="step3"></li>
+                    <li className="step0 text-muted text-center" id="step4"></li>
+                    <li className="step0 text-muted text-end" id="step5"></li>
                   </ul>
 
                   <div className="d-flex justify-content-between" id='icons-bar'>
@@ -83,6 +98,40 @@ function OrderDetails() {
               </div>
             </div>
           </div>
+
+          <form className="signup-form m-5" id='dataDisplay'>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.receptionist_id}></input>
+            <label>Receptionist_Id</label>
+          </div>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.patient_id}></input>
+            <label>patient_Id</label>
+          </div>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.services_id}></input>
+            <label>Services_Id</label>
+          </div>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.result_id}></input>
+            <label>Result_Id</label>
+          </div>
+        
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.create_time}></input>
+            <label>CreateTime</label>
+          </div>
+          <div class="input-field" id="inputField">
+            <input type="text" required value={data.status}></input>
+            <label>Status</label>
+          </div>
+      </form>
+      <div className='m-5' style={{display:"flex" , justifyContent:"center"}}>
+         <Button
+         onClick={handleClick}
+         value='Click me!'
+         />
+         </div>
         </div>
       </section>
 
