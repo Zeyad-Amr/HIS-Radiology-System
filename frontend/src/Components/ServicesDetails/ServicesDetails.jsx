af269412ce2a8dbfe83bd514feda03c6d3c9fee5
momen882001 <momen2001.mm@gmail.com>
2022-06-06 02:21:38 +0200

    ServicesDetails Structure & CSS

--- frontend/src/Components/ServicesDetails/ServicesDetails.jsx
+++ frontend/src/Components/ServicesDetails/ServicesDetails.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Services.css'
 
 function ServicesDetails() {
   const data = [
@@ -8,41 +9,41 @@ function ServicesDetails() {
       dep_name: "Radiology",
       device_name: "CT",
     },
-    {
-        name: "cat scan",
-        cost: 5000,
-        dep_name: "Radiology",
-        device_name: "CT",
-    }
   ];
   return (
     <form className="signup-form">
-      <div className="input-field-cont">
+    <div className="serv-cont">
+      <div className="text" style={{    textAlign: "center",
+        fontSize: "xx-large"}}>Services</div>
+      <div class="input-field">
+          <input type="text" required value={data[0].name}></input>
+          <label>Name</label>
+        </div>
         <div class="input-field">
-          <label className="input-field-cont-1">Department</label>
+          <label>Department</label>
           <select class="gender-selection">
             <option value="Choose Department" disabled selected hidden>
-              {data.dep}
+              {data[0].dep_name}
             </option>
             <option value="Radiology">Radiology</option>
             <option value="Lab">Lab</option>
           </select>
         </div>
         <div class="input-field">
-          <input type="text" required value={data.cost}></input>
-          <label>Cost</label>
+          <input type="text" required value={data[0].cost}></input>
+          <label >Cost</label>
         </div>
         <div class="input-field">
-          <label className="input-field-cont-1">Shift</label>
+          <label>DeviceName</label>
           <select class="gender-selection">
-            <option value="Choose Shift" disabled selected hidden>
-              {data.shift}
+            <option value="Choose DeviceName" disabled selected hidden>
+              {data[0].device_name}
             </option>
-            <option value="Day">Day</option>
-            <option value="Night">Night</option>
+            <option value="CT">CT</option>
+            <option value="MRI">MRI</option>
           </select>
         </div>
-      </div>
+        </div>
     </form>
   );
 }
