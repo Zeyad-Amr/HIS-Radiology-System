3ea5891eb8c3aa5bb4cc322a68ffc62a9d1d9dde
momen882001 <momen2001.mm@gmail.com>
2022-06-07 08:05:41 +0200

    taswee7aaaaaaa

--- frontend/src/modules/Home/Records/Records.jsx
+++ frontend/src/modules/Home/Records/Records.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CountUp from 'react-countup';
 
 function Records() {
   return (
@@ -9,22 +10,22 @@ function Records() {
                 <div class="container-stats">
                     <div class="box">
                         {/* <i class="far fa-user fa-2x fa-fw"></i> */}
-                        <span class="number" data-goal="150">0</span>
+                        <span class="number" data-goal="150"><CountUp end={500} duration={9} /></span>
                         <span class="text">Patients</span>
                     </div>
                     <div class="box">
                         {/* <i class="fas fa-code fa-2x fa-fw"></i> */}
-                        <span class="number" data-goal="135">0</span>
+                        <span class="number" data-goal="135"><CountUp end={350} duration={9} /></span>
                         <span class="text">Medical Beds</span>
                     </div>
                     <div class="box">
                         {/* <i class="fas fa-globe-asia fa-2x fa-fw"></i> */}
-                        <span class="number" data-goal="50">0</span>
+                        <span class="number" data-goal="50"><CountUp end={40} duration={9}/></span>
                         <span class="text">Doctors</span>
                     </div>
                     <div class="box">
                         {/* <i class="far fa-money-bill-alt fa-2x fa-fw"></i> */}
-                        <span class="number" data-goal="500">0</span>
+                        <span class="number" data-goal="500"><CountUp end={20} duration={9}/></span>
                         <span class="text">Winning Awards</span>
                     </div>
                 </div>
