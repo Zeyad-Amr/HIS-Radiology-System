5ff6b6addaa43931c4ee7c8e56321f3d118b46c8
Ahmed Abd El-Raouf <araouf481@hotmail.com>
2022-06-07 06:56:26 +0200

    edit  service

--- backend/controllers/servicesController.js
+++ backend/controllers/servicesController.js
@@ -3,22 +3,33 @@ const { db } = require("../mysql-con");
 module.exports = {
   //get all services
   getAllServices: (req, res) => {
-    db.query(`SELECT * FROM services`, (err, result) => {
-      if (err) return res.status(400).send(err);
-      if (result.length === 0)
-        return res.status(404).json({ message: "Not Found" });
-      res.status(200).send(result);
-    });
+    db.query(
+      `SELECT s.id, s.name, s.cost, dep.name as depName ,d.name as deviceName, d.id as deviceId FROM services s
+    LEFT JOIN dep ON s.dep_id = dep.id
+    LEFT JOIN device d ON s.device_id = d.id`,
+      (err, result) => {
+        if (err) return res.status(400).send(err);
+        if (result.length === 0)
+          return res.status(404).json({ message: "Not Found" });
+        res.status(200).send(result);
+      }
+    );
   },
   //get one services
   getOneService: (req, res) => {
     const id = req.params.id;
-    db.query(`SELECT * FROM services WHERE id = ?`, id, (err, result) => {
-      if (err) return res.status(400).send(err);
-      if (result.length === 0)
-        return res.status(404).json({ message: "Not Found" });
-      res.status(200).json(result[0]);
-    });
+    db.query(
+      `SELECT s.id, s.name, s.cost, dep.name as depName ,d.name as deviceName, d.id as deviceId FROM services s
+    LEFT JOIN dep ON s.dep_id = dep.id
+    LEFT JOIN device d ON s.device_id = d.id WHERE s.id = ?`,
+      id,
+      (err, result) => {
+        if (err) return res.status(400).send(err);
+        if (result.length === 0)
+          return res.status(404).json({ message: "Not Found" });
+        res.status(200).json(result[0]);
+      }
+    );
   },
   //add service
   createService: (req, res) => {
